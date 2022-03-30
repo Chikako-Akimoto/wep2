53e70ff5feb2fe60e9a4e6721c668eb21f045235
chikako-akimoto <akiki1995@gmail.com>
2022-03-30 19:44:53 +0900

    3/30

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[name client productno cont deliverydate mt workschedule]
+    %w[name client productno cont deliverydate mt workschedule updated_at]
 end
 
 def self.ransackable_associations(auth_object = nil)
